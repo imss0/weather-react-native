d3daab978c61bf60f69526fde80f873b8a2fc75c
sohyun <shlim0410@gmail.com>
2023-04-17 23:52:51 +0100

    initial layout

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Hello! I made a reactive native app! amazing!
-      </Text>
-      <StatusBar style="light" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>London</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temperature}>12</Text>
+          <Text style={styles.description}>Cloudy</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,12 +19,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "lightgray",
-    alignItems: "center",
-    justifyContent: "center",
   },
-  text: {
-    fontSize: 28,
-    color: "blue",
+  city: {
+    flex: 1.2,
+    backgroundColor: "#FCEDDA",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 60,
+    fontWeight: "800",
+    marginTop: 40,
+  },
+  weather: {
+    flex: 3,
+    backgroundColor: "#EE4E34",
+  },
+  day: {
+    flex: 1,
+    marginTop: 50,
+    alignItems: "center",
+  },
+  temperature: {
+    fontSize: 108,
+    fontWeight: "800",
+    marginBottom: -20,
+  },
+  description: {
+    fontSize: 48,
+    fontWeight: "300",
   },
 });
